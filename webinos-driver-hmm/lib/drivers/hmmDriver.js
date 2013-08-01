31dd6087c20870e060fe6b4a8a7dbbd0cfc8713f
IGlautier <ivanglautier@gmail.com>
2013-08-01 12:18:12 +0100

    Update hmmDriver.js

--- webinos-driver-hmm/lib/drivers/hmmDriver.js
+++ webinos-driver-hmm/lib/drivers/hmmDriver.js
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  * 
- * Copyright 2012 Telecom Italia SpA
+ * Copyright 2013 nquiring minds
  * 
  ******************************************************************************/
 var http = require("http");
